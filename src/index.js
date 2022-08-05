f8cb3d47b75a49699aced0eb318e004d0671b6f6
jiangml <jiangml@bsfit.com.cn>
2022-08-05 23:56:24 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+setInterval(tick,4000)
+
+function tick(){
+    const element = (
+        <div>
+            <h1>{new Date().toLocaleString()}</h1>
+        </div>
+    );
+    ReactDOM.render(element,document.getElementById("test"))
+}
 
 
-ReactDOM.render("<h1>hello</h1>",document.getElementById("root"))
 
