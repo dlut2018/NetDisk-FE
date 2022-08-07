83541a18acb90007e547c830e425b191d6078f4f
jiangml <jiangml@bsfit.com.cn>
2022-08-07 23:46:55 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
 
-setInterval(tick,4000)
 
 function tick(){
     const element = (
         <div>
+            <h1>login page</h1>
             <h1>{new Date().toLocaleString()}</h1>
         </div>
     );
-    ReactDOM.render(element,document.getElementById("test"))
+    ReactDOM.render(element,document.getElementById("root"))
 }
-
-
+setInterval(tick,1000)
 
